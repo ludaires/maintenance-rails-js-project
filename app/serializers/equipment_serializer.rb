345bd997199e35275a63cead719a0231d27a0d41
Ludmilla Aires Negri <ludaires@gmail.com>
2019-06-26 19:50:07 -0400

    add more attributes to the equipment serializer class.

--- app/serializers/equipment_serializer.rb
+++ app/serializers/equipment_serializer.rb
@@ -1,4 +1,4 @@
 class EquipmentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :code, :location, :uncertainty, :calibration_unit
+  attributes :id, :name, :code, :location, :calibration_unit, :calibration_start, :calibration_end, :uncertainty
   has_many :maintenances
 end
