2d2802e5c895485805a51307463e3cd1c38a2bbc
Ludmilla Aires Negri <ludaires@gmail.com>
2019-06-28 16:57:52 -0400

    refactore code.

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -2,16 +2,11 @@ class EquipmentController < ApplicationController
     before_action :set_user, only: [:index, :show, :new, :create] 
 
     def index
-        # @user = current_user
         @all_equipment = Equipment.all
-        # respond_to do |f|
-        #     f.html { render :index}
-        #     f.json {render json: @all_equipment}
         render json: @all_equipment.to_json, status: 200
     end
 
     def show
-        # @user = current_user.id
         @equipment = Equipment.find_by(id: params[:id])
         render json: @equipment, status: 200
     end
